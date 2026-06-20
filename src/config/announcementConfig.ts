cb9d5ca36d8681587c9fb9ca4f3d776bd4d39cb9
sukabing <2113097922@qq.com>
2026-06-20 22:38:05 +0800

    chore(content): sync main@93b0f3d

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -3,7 +3,7 @@ import type { AnnouncementConfig } from "../types/config";
 // 公告栏配置
 export const announcementConfig: AnnouncementConfig = {
   title: "公告", // 公告标题，填空使用i18n字符串Key.announcement
-  content: "请查看 欢迎来到LTw", // 公告内容
+  content: "请查看 欢迎来到LTW", // 公告内容
   closable: false, // 允许用户关闭公告
   link: {
     enable: true, // 启用链接
