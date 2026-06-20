5b6394f0aad042a33dccee2e24167427e72d1e2c
sukabing <2113097922@qq.com>
2026-06-20 22:37:15 +0800

    chore(content): sync main@93b0f3d

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -3,12 +3,12 @@ import type { AnnouncementConfig } from "../types/config";
 // 公告栏配置
 export const announcementConfig: AnnouncementConfig = {
   title: "公告", // 公告标题，填空使用i18n字符串Key.announcement
-  content: "你好", // 公告内容
+  content: "请查看 欢迎来到LTw", // 公告内容
   closable: false, // 允许用户关闭公告
   link: {
     enable: true, // 启用链接
-    text: "Learn More", // 链接文本
-    url: "/about/", // 链接 URL
+    text: "点这里", // 链接文本
+    url: "/posts/frist_welcome/", // 链接 URL
     external: false, // 内部链接
   },
 };
